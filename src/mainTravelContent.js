9c49a6d6cdeece992d6075e91ffe5649559d46e3
evgeniia_sindiukova <janedream777@gmail.como>
2020-09-28 11:55:08 -0400

    changes

---
+++ src/mainTravelContent.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import travelImage from './travel_main_bkg.svg';
+
+
+class mainContent extends React.Component {
+    render() {
+      return (
+        <div className="main-content">
+                    <article className="main-text">
+                        <h2>Where do you want to <span>explore?</span></h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci beatae nisi asperiores molestias quas quasi vel voluptatibu.</p>
+                        <div className="booking-btn">
+                            <button className="btn-ticket">Lets Fly</button>
+                            <button className="btn-book">Book Now</button>
+                        </div>
+                    </article>
+                    <div className="travel-img">
+                        <img className="travel-img" src={travelImage} alt="Tourist Attractions" />
+                    </div>
+        </div>
+      )
+    }
+  }
+
+
+export default mainContent;
---
+++ src/navigation.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import navIcon from './nav-btn.png';
+
+
+class Navigation extends React.Component {
+    render() {
+      return (
+        <div className="header-info">
+        <nav className="main-nav">
+            <h2 className="hidden">Navigation for Travel page</h2>
+            <div className="nav-image">
+                <img src={navIcon} alt="navigation image"  />
+            </div>
+            <ul>
+                <li><a href="#" className="nav-link">Home</a></li>
+                <li><a href="#" className="nav-link">About Us</a></li>
+                <li><a href="#" className="nav-link">Travel</a></li>
+                <li><a href="#" className="nav-link">Schedule</a></li>
+                <li><a href="#" className="nav-link">Contact Us</a></li>
+            </ul>
+         </nav>
+         <div className="login-sign-btn">
+         <button className="btn-login">Login</button>
+         <button className="btn-sign-up">Sign Up</button>
+         </div>
+         </div>
+      )
+    }
+  }
+
+
+export default Navigation;
